de2c1d91d307b429efb187b59ada6d74ee0abdd3
Simon Seyer <sey1fr@bosch.com>
2021-04-09 17:10:12 +0200

    Initial Commit

---
+++ Paraquip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paraquip
+//
+//  Created by Simon Seyer on 09.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Paraquip/ParaquipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParaquipApp.swift
+//  Paraquip
+//
+//  Created by Simon Seyer on 09.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ParaquipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
