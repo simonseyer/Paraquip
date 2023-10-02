9b169498f82020a0b808141cc7fac5787f7a58d5
Simon Seyer <simon.seyer@gmail.com>
2023-10-02 11:31:21 +0200

    Create Performance tab view

---
+++ Paraquip/View/Performance/PerformanceView.swift
@@ -0,0 +1,43 @@
+//
+//  PerformanceView.swift
+//  Paraquip
+//
+//  Created by Simon Seyer on 22.09.23.
+//
+
+import SwiftUI
+
+struct PerformanceView: View {
+
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)])
+    private var profiles: FetchedResults<Profile>
+
+    @State private var selectedProfile: Profile?
+
+    var body: some View {
+        NavigationSplitView {
+            List(selection: $selectedProfile) {
+                ForEach(profiles) { profile in
+                    NavigationLink(value: profile) {
+                        Label(profile.profileName,
+                              systemImage: profile.profileIcon.systemName.removingFill)
+                        .foregroundStyle(.primary)
+                    }
+                }
+            }
+            .navigationTitle("Performance")
+        } detail: {
+            if let selectedProfile {
+                ProfileWeightView(profile: selectedProfile)
+            } else {
+                ContentUnavailableView("Select an equipment set",
+                                       systemImage: "tray.full.fill")
+            }
+        }
+    }
+}
+
+#Preview {
+    PerformanceView()
+        .environment(\.managedObjectContext, .preview)
+}
